f30be516704fbcd0a5aae3a7237fed7f8f488bd1
Ubuntu <ubuntu@ip-172-31-29-36.ap-southeast-2.compute.internal>
2025-03-31 16:49:54 +1100

    Tracking AWS resource usage

--- aws_resource_tracker.sh
+++ aws_resource_tracker.sh
@@ -14,3 +14,11 @@ aws iam list-users
 echo "list aws lambda function"
 aws lambda list-functions
 
+# We can use cronjob to execute a script in a given certain time 
+# Type command crontab -e to open crontab editor
+
+# Add the following line 
+#34 16 * * * /home/ubuntu/DevOps/aws_resource_tracker.sh >> /home/ubuntu/DevOps/log.txt
+
+#This will generate output in log.txt everyday at 4:34 pm.
+#If there is no log.txt file, it will autoatically added log.txt
