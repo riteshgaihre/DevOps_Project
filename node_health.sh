d31d549368228f95908da80bb573b223c270f6b4
Ubuntu <ubuntu@ip-172-31-6-23.ap-southeast-2.compute.internal>
2025-04-20 19:18:23 +1000

    Script updated inside node_health.sh

--- node_health.sh
+++ node_health.sh
@@ -4,9 +4,9 @@
 #Date - Sat 19 Apr 2025
 #Script - This script provides a node health of virtual machine.
 
-#set -x           #debug mode
-#set -e           #exit the script when there is an error
-#set -o pipefail  #fail the script if any part of pipeline fails
+#set -x          #debug mode
+set -e           #exit the script when there is an error
+set -o pipefail  #fail the script if any part of pipeline fails
 
 echo "print the running processes then search string name amazon using grep and extract specific columns number 2 using awk"
 ps -ef | grep amazon | awk -F " " '{print $2}'
